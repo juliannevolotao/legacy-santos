b438a44a54b7102d61b5a89cf5c200078a67a392
Julianne <julivolotao@gmail.com>
2025-06-10 22:22:24 -0300

    Init develop branch

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ function App() {
     <>
       <div className={styles.container}>
       <h1 className={styles.title}>LGCY CONF 25</h1>
-      <a href="https://articket.com.br/e/4095/legacy-conf" target="_blank" rel="noopener noreferrer">
-        <button className={styles.button}>Garanta seu ingresso</button>
-      </a>
+      {/* <a href="https://articket.com.br/e/4095/legacy-conf" target="_blank" rel="noopener noreferrer">
+        <button className={styles.button}>Desenvolvimento</button>
+      </a> */}
     </div>
     </>
   )
